c7d4f96691e754298fbcc245d294de28f2dafa0c
DevIlyasJrny <elbourari11@gmail.com>
2024-02-04 17:57:37 +0200

    simple password-generator.py

---
+++ password-generator.py
@@ -0,0 +1,52 @@
+#define character sets
+#determine password length
+#create the pattern
+#randomization to enhance security
+#Assemble the password
+# The password at the end should BE IN ORDER OF : LETTER NUMBER SYMBOL 
+
+import random
+import string
+
+#letters set(upper and lower case)
+letters = string.ascii_letters
+
+#numbers set
+numbers = string.digits
+
+#special characters set
+special_characters = "!@#$%^&*()-_=+[]{};:',.<>/?"
+
+
+#determine password length
+def the_password_length():
+    while True:
+        try:
+            length = int(input("How long do you want your password to be? "))
+            if 8 <= length <= 15:
+                return length
+            else:
+                print("Password length must be between 8 and 15 characters")
+        except:
+            print("Invalid input. Please enter a valid integer.")
+
+
+
+def the_generate_password(length):
+    password = ""
+    for i in range(length):
+        if i % 3 == 0:
+            # Add a random letter
+            password += random.choice(string.ascii_letters)
+        elif (i + 2) % 3 == 0:
+            # Add a random number
+            password += random.choice(string.digits)
+        else:
+            # Add a random special character
+            special_characters = "!@#$%^&*()-_=+[]{};:',.<>/?"
+            password += random.choice(special_characters)
+    return password
+
+password_length = the_password_length() 
+generated_password = the_generate_password(password_length)
+print("Generated Password:", generated_password)
