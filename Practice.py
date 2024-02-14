8c2a47c6fc4005205e80d23d97a7b51b841dea9c
Ilyas El <elbourari4@gmail.com>
2024-02-14 16:22:48 +0200

    define trade function and call it inside our treasure fct and also inventory

--- Practice.py
+++ Practice.py
@@ -1,5 +1,7 @@
 import random
 
+#we will add an inventory for a global access:
+inventory = []
 
 #
 def start_game():
@@ -8,21 +10,33 @@ def start_game():
     print(f"Let the adventure begin, {name}!")
     choose_path(name)
 
-
-#
+#Adiing another function for more interactivity:
+def trade_with_merchant(name, treasure):
+    print(f"A mysterious merchant appears offering a trade for your {treasure}.")
+    trade = input(f"Do you want to trade your {treasure} with the merchant for a magical shield for the journey? (yes/no): ").lower()
+    if trade == "yes":
+        print(f"Congrats {name}, you traded {treasure} for a magical shield!")
+        #inventory to append the item:
+        inventory.append('magical shield')   
+    else:
+        print(f"{name} decided to keep the {treasure}.")        
+        
+#Updating 
 def encounter_treasure(name):
-    treasure = random.choice(['diamonds', 'a sword', 'a secret map'])
+    treasures = ['diamonds', 'ancient sword', 'secret map']
+    treasure = random.choice(treasures)
     print(f"Oh !! Congrats, {name} You've encountered a treasure that has {treasure}")
-
+    # we have to call trade function here for a batter inter.:
+    trade_with_merchant(name, treasure)
 
 
 #
 def encounter_monster(name):
     monster = random.choice(['dragon', 'friendly dragon', 'pirate'])
-    print(f"Oh no, {name}You've encountered a {monster}!")    
+    print(f"Oh no, {name} You've encountered a {monster}!")    
 
 def choose_path(name):
-    path = input(f"You are in crossroad {name}. Would you like to go left or right? (left/right): ")
+    path = input(f"You are in crossroad {name}. Would you like to go left or right? (left/right): ").lower()
     if path == 'left':
         encounter_treasure(name)
     elif path == 'right':
